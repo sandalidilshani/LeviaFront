ce6d553d891e9777e49a1d786cc3b436f8f62c7f
sandalidilshani <79295251+sandalidilshani@users.noreply.github.com>
2024-07-10 12:18:37 +0530

    backend

--- src/pages/shared/Login.js
+++ src/pages/shared/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3009/auth/login", {
+      const response = await axios.post("http://leviabackend-production.up.railway.app/auth/login", {
         userName,
         userpassword,
       });
